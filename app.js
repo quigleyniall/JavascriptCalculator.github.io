d5003ab3508a39644a9668c325e3e6171a52a969
Niall Quigley <37251187+quigleyniall@users.noreply.github.com>
2018-03-19 20:03:44 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,55 @@
+    $(document).ready(function(){
+    //stores input
+    var inputs=[""];
+    var totalstring;
+    var operators1=["+","-","/","*"];
+    var operators2=["."];
+    var nums=[0,1,2,3,4,5,6,7,8,9];
+
+    function getValue(input){
+        if(operators2.includes(inputs[inputs.length-1])===true && input==="."){
+            console.log("Duplicate '.' ");
+        }
+        else if(inputs.length===1 && operators1.includes(input)===false){
+            inputs.push(input);
+        }
+        else if(operators1.includes(inputs[inputs.length-1])===false){
+            inputs.push(input);
+        }
+        else if(nums.includes(Number(input))){
+            inputs.push(input);
+        }
+        update();
+    }
+
+    function update(){
+        totalstring=inputs.join("");
+        $("#steps").html(totalstring);
+    }
+    function getTotal(){
+        totalstring=inputs.join("");
+        $("#steps").html(eval(totalstring));
+    }
+    $("button").on("click",function(){
+        if(this.id==="ac"){
+            inputs=[""];
+            update();
+        }
+        else if(this.id==="ce"){
+            inputs.pop();
+            update();
+        }
+        else if(this.id==="total"){
+            getTotal();
+        }
+        else{
+            if(inputs[inputs.length-1].indexOf("+","-","/","*")===-1){
+                getValue(this.id);
+            }
+            else{
+                getValue(this.id);
+            }
+        }
+
+    });
+});
